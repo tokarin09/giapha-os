ccffc6fed0a10223c18f87081bb2a0cb68ad023d
tokarin09 <96907987+tokarin09@users.noreply.github.com>
2026-04-29 12:28:47 +0700

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  siteName: process.env.SITE_NAME || "Gia Phả OS",
+  siteName: process.env.SITE_NAME || "Gia Phả Chi Cụ Nguyễn Văn Đàm",
   exampleEmail: process.env.EXAMPLE_EMAIL || "",
   examplePassword: process.env.EXAMPLE_PASSWORD || "",
   demoDomain: process.env.DEMO_DOMAIN || "giapha-os.homielab.com",
